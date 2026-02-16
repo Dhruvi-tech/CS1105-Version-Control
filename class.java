dd3d0e9ffa15a7ad9c6fc87f3de56a3e41832242
Dhruvi Mittal <dhruvimittalbsc24@rvu.edu.in>
2026-02-16 16:04:31 +0530

    Add basic TestApp demo

---
+++ class.java
@@ -0,0 +1,8 @@
+class TestApp {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Test run: " + a + " + " + b + " = " + sum);
+    }
+}
