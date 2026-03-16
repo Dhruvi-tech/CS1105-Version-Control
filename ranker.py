bc55a37b70d3b6963f2678a931f5aaa273442de5
Dhruvi Mittal <dhruvimittalbsc24@rvu.edu.in>
2026-03-16 12:01:05 +0530

    Delete ranker.py

--- ranker.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Enter first value: "))
-b = float(input("Enter second value: "))
-c = float(input("Enter third value: "))
-
-largest = max(a, b, c)
-
-print("Largest value:", largest)
